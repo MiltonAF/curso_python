19f60c3c9debe5ca6f7e3f53f7b1f6fc43abc2d9
MiltonAF <mafelizzolam@gmail.com>
2023-07-05 20:44:51 -0500

    vemos los datos simples de python

---
+++ tipos de datos/datos-simples.py
@@ -0,0 +1,18 @@
+#Escribir texto en una linea usar comillas normal
+"string"
+'string'
+
+#Escribir texto en multiple linea usar comillas triple 
+"""string"""
+'''string'''
+
+#int (Numeros enteros)
+4
+
+#float (Numeros flotatantes: se remplaza la coma por el punto)
+4.4
+
+#BOOL (Valores booleanos: True o False)
+False
+True
+
