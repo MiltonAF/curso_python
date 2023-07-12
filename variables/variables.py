dbe4f4cc1a12438336b665e59a4b72c790f2839f
MiltonAF <mafelizzolam@gmail.com>
2023-07-11 22:37:56 -0500

    declaramos una variable

---
+++ variables/variables.py
@@ -0,0 +1,23 @@
+a = 2
+b = 3
+c = a + b
+
+#definiendo una varible con calmeCase
+nombreCompleto = "el nombre"
+
+#definiendo una variable estilo snake-case
+nombre_completo = "el nombre"
+
+#concatenar con f-strings
+nombre = f"Milton Felizzola {b}"
+
+#concatenar con +
+bienvenida = "hola" + " ¿como estas?"
+
+#un operador para borrar datos
+del a
+
+
+#operadores de pertenencias (in / not in)
+print("M" in nombre)
+print("M" not in nombre)
