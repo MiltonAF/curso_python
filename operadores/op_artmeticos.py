7836251b3e6a0f1c5f8ed7583bf2cdb1be3a3f04
MiltonAF <mafelizzolam@gmail.com>
2023-07-23 21:49:16 -0500

    operadores aritmerticos

---
+++ operadores/op_artmeticos.py
@@ -0,0 +1,21 @@
+#suma y resta (+ & -)
+suma = 12 + 5
+resta = 12 - 5
+
+#Multiplicacion y divicion  (* & /)
+multi = 12 * 5
+division = 12 / 5 #nos devuelve un datos float ( flotante )
+
+#potenciancion exponente (**)
+exponente = 12**5
+
+#Division baja (//)
+division_baja = 12//5 #nos devuelve un entero redondeado hacia abajo
+
+#resto o modulo (%)
+mod = 12%5 #nos devuelve el residuo de la division
+
+#type nos devulve que tipo de datos es
+tipo_de_datos = type(division_baja)
+
+print(tipo_de_datos)
