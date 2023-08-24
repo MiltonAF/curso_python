26760beea576f3b3a4e304be7d0a605f8a5fec8b
MiltonAF <mafelizzolam@gmail.com>
2023-08-23 22:06:46 -0500

    else if

---
+++ condicionales/else-if.py
@@ -0,0 +1,8 @@
+sueldo = 10000
+
+if sueldo >=50000 & sueldo < 70000:
+    print("esta bien en latino america")
+elif sueldo >=70000:
+    print("esta bien el mundo")
+else:
+    print("no esta bien")
