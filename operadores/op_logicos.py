800ba135ba44e017949d216517e0a7da4a0761b2
MiltonAF <mafelizzolam@gmail.com>
2023-08-26 16:53:31 -0500

    Operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,19 @@
+#AND
+
+R1 = True & True #devuelve True
+R2 = True & False #devuelve False
+R3 = False & True #decuelve False
+R4 = False & False #devuelve False
+
+#OR
+
+R5 = True | True # devulve True
+R6 = True | False # devuelve True
+R7 = False | True # Devuelve True
+R8 = False | False # Devuelve False
+
+#NOT
+
+R9 = not True # Devuelve False
+R10 = not False # Devuelve True
+
