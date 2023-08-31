1e7cbdfca7e6973f7eb7667abf414a5f77c7ff22
MiltonAF <mafelizzolam@gmail.com>
2023-08-30 23:04:27 -0500

    manejo de inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,12 @@
+#pedir un dato al usuario y guardarlo en la varible23
+numero = input("dame un numero")
+
+#convertimos el resultado a int
+res = int(numero) * 2
+
+#convertivos el resultado a float
+res2 = float(numero) * 2
+
+
+print(res2)
+
