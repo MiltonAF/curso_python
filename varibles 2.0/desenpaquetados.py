7b52f1b082fbe13da85761b396615baf75286151
MiltonAF <mafelizzolam@gmail.com>
2023-08-30 23:35:03 -0500

    desempaquetados

---
+++ varibles 2.0/desenpaquetados.py
@@ -0,0 +1,8 @@
+#Creando datos
+datos_en_tupla = ("Milton", "Felizzola", 23)
+datos_en_lista = ["Milton", "Felizzola", 23]
+
+#Desempaquetamos
+nombre, apellido, edad = datos_en_tupla
+
+print(nombre)
