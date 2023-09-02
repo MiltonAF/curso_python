0c43d620678c6699eee8e35bb1401f9a778e97fa
MiltonAF <mafelizzolam@gmail.com>
2023-09-02 18:05:25 -0500

    funciones integradas de python

---
+++ funciones/build_in.py
@@ -0,0 +1,25 @@
+numeros = [4,7,1,42,15]
+
+#encontrado el numero mayor de la lista
+numero_mas_alto = max(numeros)
+
+
+#encontrar el numero menor de la lista
+numero_mas_bajo = min(numeros)
+
+
+#redondeando a 6 decimales
+numero = round(12.3456, 2)
+
+
+#retorna False -> 0, Vacio, False, None / True -> distinto a cero, texto, datos no vacio, 
+resultado_bool = bool(8)
+
+
+#retorna True, si todos los valores son verdaderos
+resultado_all = all([1,'sdf',True])
+
+#suma todos los valores de un iterable
+suma_total = sum(numeros)
+
+print(suma_total)
