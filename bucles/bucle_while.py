33714d28fe758629ee2d83d92e24df1ffb2fd134
MiltonAF <mafelizzolam@gmail.com>
2023-09-01 22:22:49 -0500

    bucle while

---
+++ bucles/bucle_while.py
@@ -0,0 +1,8 @@
+#creando un contador que va a ir sumandose
+contador = 0
+
+#Mientra que la condicion se cumpla, el bucle se va a ejecutar 
+#(vuelta, tras vuelta se verifica la condiccion)
+while contador < 10:
+    print(contador)
+    contador+= 1
